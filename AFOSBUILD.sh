30735c4ece80cd490a538f39cfbca361f49901d5
Weidsom Nascimento <source@snakesecurity.org>
2025-04-10 12:15:53 -0700

    Update bettercap source code to 2.41.0

---
+++ AFOSBUILD.sh
@@ -0,0 +1,25 @@
+make
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "make bettercap... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+make install
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Make install bettercap... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+bettercap -eval "caplets.update; ui.update; q"
+
+cp -Rf replace/* /usr/local/share/bettercap/caplets/
